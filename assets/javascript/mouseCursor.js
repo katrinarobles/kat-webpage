347b72e48b005efae034b31ee65073bdc0f652a3
Katrina Robles <katrinarobles@live.com>
2020-11-29 17:05:53 +0100

    added jquery

--- assets/javascript/mouseCursor.js
+++ assets/javascript/mouseCursor.js
@@ -1,17 +1,3 @@
-// function mouseCursor() {
-//   var element = document.querySelector(".flexy-container");
-//   var cursor = document.querySelector(".cursor");
-//   element.mousemove(function (e) {
-//     cursor.show().css({
-//       "left": e.clientX,
-//       "top": e.clientY
-//     });
-//   }).mouseout(function () {
-//     cursor.hide();
-//   });
-// }
-
-
 var cont = document.getElementById('pewpew');
 cont.onmousemove = applyCustomPointer;
 var customPointer = document.getElementById('customPointer');
